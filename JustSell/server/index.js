a3e843c7c7c9d17f24d453fcd44f390b93aa0482
Bo Kyung Kim <you@example.com>
2024-01-25 11:39:27 -0500

    revised index.js in server folder

--- JustSell/server/index.js
+++ JustSell/server/index.js
@@ -1,20 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
-app.use(express.json());
-app.use(cors());
-
-const db = require("./models");
-
-//Routers
-const usersRouter = require("./routes/Users");
-app.use("/auth", usersRouter);
-const todosRouter = require("./routes/Todos");
-app.use("/todo", todosRouter);
-
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("Server is running on port 3001");
-  });
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
 });
