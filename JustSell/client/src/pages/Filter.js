84784a61d337c417f9cc3788a53385f9e971958b
xingr121 <137085577+xingr121@users.noreply.github.com>
2024-01-26 14:25:35 -0500

    Update Filter.js

--- JustSell/client/src/pages/Filter.js
+++ JustSell/client/src/pages/Filter.js
@@ -1,102 +1,172 @@
-import React from "react";
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Filter() {
+  let navigate = useNavigate();
+  const [listOfProperties, setListProperties] = useState([]);
+  const initialValues = {
+    location: "",
+    sellOption: "sale",
+    propertyType: "apartment",
+    bedrooms: "1",
+    minBudget: "0",
+    maxBudget: "50000",
+  };
+  const validationSchema = Yup.object().shape({
+    location: Yup.string().max(50, "Must be 50 characters or less"),
+  });
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3001/properties/search",
+        { params: data }
+      );
+      setListProperties(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <session className="filters" style={{ paddingBottom: 0 }}>
-      <form action="" method="post">
-        <div id="close-filter">
-          <i class="fas fa-times"></i>
-        </div>
-        <h3>Filter your search</h3>
-        <div class="flex">
-          <div class="box">
-            <p>Location</p>
-            <input
-              type="text"
-              name="Location"
-              required
-              maxlength="50"
-              placeholder="Search by city or Postal code"
-              class="input"
-            />
-          </div>
-          <div class="box">
-            <p>Offer type</p>
-            <select name="sellOption" class="input" required>
-              <option value="sale" selected>
-                Sale
-              </option>
-              <option value="resale">Resale</option>
-              <option value="leasing">Leasing</option>
-            </select>
-          </div>
-          <div class="box">
-            <p>Property type</p>
-            <select name="PropertyType" class="input" required>
-              <option value="apartment" selected>
-                Apartment
-              </option>
-              <option value="house">House</option>
-              <option value="duplexTriplex">Duplex or Triplex</option>
-              <option value="condo">Condo</option>
-              <option value="comercialBuilding">Comercial Building</option>
-            </select>
-          </div>
-          <div class="box">
-            <p>Bedrooms</p>
-            <select name="Bedrooms" class="input" required>
-              <option value="1" selected>
-                1
-              </option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6+</option>
-            </select>
-          </div>
-          <div class="box">
-            <p>Minimum budget</p>
-            <select name="min" class="input" required>
-              <option value="0">0</option>
-              <option value="50000" selected>
-                50k
-              </option>
-              <option value="100000">100k</option>
-              <option value="150000">150k</option>
-              <option value="200000">200k</option>
-              <option value="300000">300k</option>
-              <option value="400000">400k</option>
-              <option value="450000">450k</option>
-              <option value="500000">500k</option>
-              <option value="1000000">1M</option>
-            </select>
-          </div>
-          <div class="box">
-            <p>Maximum budget</p>
-            <select name="max" class="input" required>
-              <option value="50000" selected>
-                50k
-              </option>
-              <option value="100000">100k</option>
-              <option value="150000">150k</option>
-              <option value="200000">200k</option>
-              <option value="300000">300k</option>
-              <option value="400000">400k</option>
-              <option value="450000">450k</option>
-              <option value="500000">500k</option>
-              <option value="1000000">1M+</option>
-            </select>
-          </div>
-        </div>
-        <input
-          type="submit"
-          value="Search Property"
-          name="filter_search"
-          class="btn"
-        />
-      </form>
-    </session>
+    <>
+      <section className="filters" style={{ paddingBottom: 0 }}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+        >
+          <Form>
+            <div id="close-filter">
+              <i className="fas fa-times"></i>
+            </div>
+            <h3>Filter your search</h3>
+            <div className="flex">
+              <div className="box">
+                <p>Location</p>
+                <Field
+                  type="text"
+                  name="location"
+                  required
+                  maxLength="50"
+                  placeholder="Search by city or Postal code"
+                  className="input"
+                />
+                <ErrorMessage
+                  name="location"
+                  component="div"
+                  className="error"
+                />
+              </div>
+              <div className="box">
+                <p>Offer type</p>
+                <Field as="select" name="sellOption" className="input" required>
+                  <option value="sale">Sale</option>
+                  <option value="resale">Resale</option>
+                  <option value="leasing">Leasing</option>
+                </Field>
+                <ErrorMessage
+                  name="sellOption"
+                  component="div"
+                  className="error"
+                />
+              </div>
+            </div>
+            <button type="submit" className="btn">
+              Search Property
+            </button>
+          </Form>
+        </Formik>
+      </section>
+
+      <section className="listings">
+        {listOfProperties.length > 0 ? (
+          <>
+            <h1 className="heading">Search Results</h1>
+            <div className="box-container">
+              {listOfProperties.map((property) => (
+                <div key={property.PropertyID} className="box">
+                  <Form action="" method="POST">
+                    <input
+                      type="hidden"
+                      name="PropertyID"
+                      value={property.PropertyID}
+                    />
+                    <div className="thumb">
+                      <img
+                        src={`images/PropertiesImages/${property.ImageFileName}`}
+                        alt="Property"
+                      />
+                    </div>
+                  </Form>
+                  <div className="box">
+                    <div className="price">
+                      <i className="fas fa-dollar-sign"></i>
+                      <span>
+                        {property.Price.toFixed(2).replace(
+                          /\d(?=(\d{3})+\.)/g,
+                          "$&,"
+                        )}
+                      </span>
+                    </div>
+                    <h3 className="name">{property.PropertyType}</h3>
+                    <p className="location">
+                      <i className="fas fa-map-marker-alt"></i>
+                      <span>{property.City}</span>
+                    </p>
+                    <div className="flex">
+                      <p>
+                        <i className="fas fa-house"></i>
+                        <span>{property.PropertyType}</span>
+                      </p>
+                      <p>
+                        <i className="fas fa-bed"></i>
+                        <span>{property.Bedrooms}</span>
+                      </p>
+                      <p>
+                        <i className="fas fa-trowel"></i>
+                        <span>{property.ConstructionStatus}</span>
+                      </p>
+                      <p>
+                        <i className="fas fa-couch"></i>
+                        <span>
+                          {property.furnished ? "furnished" : "not furnished"}
+                        </span>
+                      </p>
+                      <p>
+                        <i className="fas fa-maximize"></i>
+                        <span>{property.size}sqft</span>
+                      </p>
+                    </div>
+                    <div className="flex-btn">
+                      <button
+                        className="btn"
+                        onClick={() => {
+                          navigate(`/property/${property.PropertyID}`);
+                        }}
+                      >
+                        View Details
+                      </button>
+                      <button
+                        onClick={() => {
+                          navigate(`/property/${property.PropertyID}`);
+                        }}
+                        className="btn"
+                      >
+                        Send Offer
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          <h1 className="heading">Find your new Home!</h1>
+        )}
+      </section>
+    </>
   );
 }
 
