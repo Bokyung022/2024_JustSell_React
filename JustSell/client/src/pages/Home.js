e426c15cdddb5521ecafe371241c4636eaca8a2f
Bo Kyung Kim <you@example.com>
2024-01-26 11:30:22 -0500

    added Listings page

--- JustSell/client/src/pages/Home.js
+++ JustSell/client/src/pages/Home.js
@@ -1,32 +1,7 @@
 import React from "react";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
 function Home() {
-  const [listOfProperties, setListOfProperties] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/properties").then((response) => {
-      setListOfProperties(response.data);
-    });
-  }, []);
-  return (
-    <div>
-      {listOfProperties.map((value, key) => {
-        return (
-          <div className="property">
-            <div className="propertyID">{value.propertyID}</div>
-            <div className="propertyContents">
-              {value.streetNum} <br />
-              {value.streetName} <br />
-              {value.city} <br />
-              {value.province}
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div>This is Home Page</div>;
 }
 
 export default Home;
