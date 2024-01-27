47792ab6024921db39d14969f37618964a117633
KarinaDeVargas <134572496+KarinaDeVargas@users.noreply.github.com>
2024-01-26 20:53:46 -0500

    Added Navbar and CSS

--- JustSell/client/src/App.js
+++ JustSell/client/src/App.js
@@ -1,29 +1,28 @@
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import "./App.css";
-import CreateProperty from "./pages/CreateProperty";
-import Filter from "./pages/Filter";
-import Home from "./pages/Home";
-import Listings from "./pages/Listings";
-import Property from "./pages/Property";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import CreateProperty from './pages/CreateProperty';
+import Filter from './pages/Filter';
+import Home from './pages/Home';
+import Listings from './pages/Listings';
+import Registration from './pages/Registration';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Link to="/">Home Page</Link>
-        <Link to="/search">Search</Link>
-        <Link to="/listings">Listings</Link>
-        <Link to="/createProperty">Create Property</Link>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Filter />} />
-          <Route path="/listings" element={<Listings />} />
-          <Route path="/createProperty" element={<CreateProperty />} />
-          <Route path="/property/:id" element={<Property />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+          <div className="App">
+              <Router>
+                <Navbar />
+                 <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/search" element={<Filter />} />
+                  <Route path="/listings" element={<Listings />} />
+                  <Route path="/createProperty" element={<CreateProperty />} />
+                  {/* <Route path="/login" element={<Login />} /> */}
+                  <Route path="/registration" element={<Registration />} />
+                </Routes>
+              </Router>
+            </div>
+         
+  );  
 }
 
 
