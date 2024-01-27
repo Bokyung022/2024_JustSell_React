51380a643b045c7d314eb768ad77bbc77967b0ae
Bo Kyung Kim <you@example.com>
2024-01-27 01:24:47 -0500

    revised App.js

--- JustSell/client/src/App.js
+++ JustSell/client/src/App.js
@@ -1,26 +1,32 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./pages/Home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./component/Footer";
+import Navbar from "./component/Navbar";
 import CreateProperty from "./pages/CreateProperty";
 import Filter from "./pages/Filter";
+import Home from "./pages/Home";
 import Listings from "./pages/Listings";
+import Registration from "./pages/Registration";
+import EditProperty from "./pages/EditProperty";
 import Property from "./pages/Property";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/">Home Page</Link>
-        <Link to="/search">Search</Link>
-        <Link to="/listings">Listings</Link>
-        <Link to="/createProperty">Create Property</Link>
+        <Navbar />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Filter />} />
           <Route path="/listings" element={<Listings />} />
           <Route path="/createProperty" element={<CreateProperty />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/registration" element={<Registration />} />
           <Route path="/property/:id" element={<Property />} />
+          <Route path="/editProperty/:id" element={<EditProperty />} />
         </Routes>
+
+        <Footer />
       </Router>
     </div>
   );
