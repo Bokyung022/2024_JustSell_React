feee9ed199afd943576767cb256de7e138cf0754
Bo Kyung Kim <you@example.com>
2024-01-30 15:32:28 -0500

    registration error fixed

--- JustSell/client/src/pages/Registration.js
+++ JustSell/client/src/pages/Registration.js
@@ -7,22 +7,21 @@ import { useNavigate } from "react-router-dom";
 function Registration() {
   let navigate = useNavigate();
   const initialValues = {
-    FirstName: "",
-    LastName: "",
-    Email: "",
-    UserName: "",
-    Password: "",
-    Passcomf: "",
+    userName: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
     Phone: "",
-    StreetNum: "",
-    StreetName: "",
-    City: "",
-    Province: "",
-    Postal: "",
-    Company: "",
-    Role: "",
-    isRealtorApproved: "",
-    RealtorCertification: "",
+    streetNum: 0,
+    streetName: "",
+    city: "",
+    province: "",
+    postal: "",
+    company: "",
+    role: "",
+    isRealtorApproved: 0,
+    realtorCertification: "",
   };
 
   const validationSchema = Yup.object().shape({
@@ -49,6 +48,7 @@ function Registration() {
       .then(() => {
         // handle success
         console.log(data);
+        navigate("/");
       })
       .catch((error) => {
         // handle error
@@ -59,7 +59,6 @@ function Registration() {
   return (
     <div className="home">
       <div className="center">
-        {""}
         <Formik
           initialValues={initialValues}
           onSubmit={onSubmit}
@@ -69,71 +68,59 @@ function Registration() {
             <h3>Registration</h3>
 
             <div className="box">
-              <label htmlFor="FirstName">First Name:</label>
-              <ErrorMessage name="FirstName" component="span" />
+              <label htmlFor="firstName">First Name:</label>
+              <ErrorMessage name="firstName" component="span" />
               <Field
                 className="input"
-                id="FirstName"
-                name="FirstName"
+                id="firstName"
+                name="firstName"
                 placeholder="Enter your first name"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="LastName">Last Name:</label>
-              <ErrorMessage name="LastName" component="span" />
+              <label htmlFor="lastName">Last Name:</label>
+              <ErrorMessage name="lastName" component="span" />
               <Field
                 className="input"
-                id="LastName"
-                name="LastName"
+                id="lastName"
+                name="lastName"
                 placeholder="Enter your last name"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="Email">Email:</label>
-              <ErrorMessage name="Email" component="span" />
+              <label htmlFor="email">Email:</label>
+              <ErrorMessage name="email" component="span" />
               <Field
                 className="input"
                 type="email"
-                id="Email"
-                name="Email"
+                id="email"
+                name="email"
                 placeholder="Enter your email"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="UserName">Username:</label>
-              <ErrorMessage name="UserName" component="span" />
+              <label htmlFor="userName">Username:</label>
+              <ErrorMessage name="userName" component="span" />
               <Field
                 className="input"
-                id="UserName"
-                name="UserName"
+                id="userName"
+                name="userName"
                 placeholder="Enter your username"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="Password">Password:</label>
-              <ErrorMessage name="Password" component="span" />
+              <label htmlFor="password">Password:</label>
+              <ErrorMessage name="password" component="span" />
               <Field
                 className="input"
                 type="password"
-                id="Password"
-                name="Password"
+                id="password"
+                name="password"
                 placeholder="Enter your password"
-              />
-            </div>
-
-            <div className="box">
-              <label htmlFor="Passcomf">Confirm Password:</label>
-              <ErrorMessage name="Passcomf" component="span" />
-              <Field
-                className="input"
-                type="password"
-                id="Passcomf"
-                name="Passcomf"
-                placeholder="Confirm your password"
               />
             </div>
 
@@ -149,67 +136,67 @@ function Registration() {
             </div>
 
             <div className="box">
-              <label htmlFor="StreetNum">Street Number:</label>
-              <ErrorMessage name="StreetNum" component="span" />
+              <label htmlFor="streetNum">Street Number:</label>
+              <ErrorMessage name="streetNum" component="span" />
               <Field
                 className="input"
-                id="StreetNum"
-                name="StreetNum"
+                id="streetNum"
+                name="streetNum"
                 placeholder="Enter your street number"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="StreetName">Street Name:</label>
-              <ErrorMessage name="StreetName" component="span" />
+              <label htmlFor="streetName">Street Name:</label>
+              <ErrorMessage name="streetName" component="span" />
               <Field
                 className="input"
-                id="StreetName"
-                name="StreetName"
+                id="streetName"
+                name="streetName"
                 placeholder="Enter your street name"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="City">City:</label>
-              <ErrorMessage name="City" component="span" />
+              <label htmlFor="city">City:</label>
+              <ErrorMessage name="city" component="span" />
               <Field
                 className="input"
-                id="City"
-                name="City"
+                id="city"
+                name="city"
                 placeholder="Enter your city"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="Province">Province:</label>
-              <ErrorMessage name="Province" component="span" />
+              <label htmlFor="province">Province:</label>
+              <ErrorMessage name="province" component="span" />
               <Field
                 className="input"
-                id="Province"
-                name="Province"
+                id="province"
+                name="province"
                 placeholder="Enter your province"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="Postal">Postal Code:</label>
-              <ErrorMessage name="Postal" component="span" />
+              <label htmlFor="postal">Postal Code:</label>
+              <ErrorMessage name="postal" component="span" />
               <Field
                 className="input"
-                id="Postal"
-                name="Postal"
+                id="postal"
+                name="postal"
                 placeholder="Enter your postal code"
               />
             </div>
 
             <div className="box">
-              <label htmlFor="Company">Company:</label>
-              <ErrorMessage name="Company" component="span" />
+              <label htmlFor="company">Company:</label>
+              <ErrorMessage name="company" component="span" />
               <Field
                 className="input"
-                id="Company"
-                name="Company"
+                id="company"
+                name="company"
                 placeholder="Enter your company name"
               />
             </div>
--- JustSell/server/routes/users.js
+++ JustSell/server/routes/users.js
@@ -1,65 +1,69 @@
-//https://www.youtube.com/watch?v=OGGnjBE5qr0&list=PLpPqplz6dKxUaZ630TY1BFIo5nP-_x-nL&index=8&t=1882s
 const express = require("express");
 const router = express.Router();
-const { Users } = require("../models");
+const { users } = require("../models");
 const bcrypt = require("bcrypt");
+const { validateToken } = require("../middleware/AuthMiddleware");
+const { sign } = require("jsonwebtoken");
 
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const hash = await bcrypt.hash(password, 10);
-    await users.create({
-      username: username,
-      password: hash,
-    });
-    res.json("SUCCESS");
+    const listOfUsers = await users.findAll();
+    res.json(listOfUsers);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.post("/login", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const {
+      userName,
+      password,
+      email,
+      firstName,
+      lastName,
+      Phone,
+      streetNum,
+      streetName,
+      city,
+      province,
+      postal,
+      company,
+      role,
+      isRealtorApproved,
+      realtorCertification,
+    } = req.body;
 
-    const user = await Users.findOne({ where: { username: username } });
+    const hash = await bcrypt.hash(password, 10);
 
-    if (!user) return res.json({ error: "User Doesn't Exist" });
+    await users.create({
+      userName,
+      password: hash,
+      email,
+      firstName,
+      lastName,
+      Phone,
+      streetNum,
+      streetName,
+      city,
+      province,
+      postal,
+      company,
+      role,
+      isRealtorApproved,
+      realtorCertification,
+    });
 
-    const match = await bcrypt.compare(password, user.password);
-
-    if (!match)
-      return res.json({ error: "Wrong Username And Password Combination" });
-
-    const accessToken = sign(
-      { username: user.username, id: user.id },
-      "importantsecret"
-    );
-    res.json({ token: accessToken, username: username, id: user.id });
+    res.json("SUCCESS");
   } catch (error) {
-    console.error("Error during login:", error);
+    console.error("Error creating user:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 router.get("/auth", validateToken, (req, res) => {
   res.json(req.user);
-});
-
-router.get("/basicinfo/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const basicInfo = await users.findByPk(id, {
-      attributes: { exclude: ["password"] },
-    });
-
-    res.json(basicInfo);
-  } catch (error) {
-    console.error("Error fetching basic info:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
 });
 
 module.exports = router;
